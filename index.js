3e1299ee91f09d97810c9c4492ac3ad80b3e02c1
foxl <every.flipped@gmail.com>
2022-11-06 19:23:54 +0000

    added database status + refresh button

--- index.js
+++ index.js
@@ -4,23 +4,24 @@ function load() {
         document.getElementById('btw').setAttribute('onclick', "loadPage('library', null)");
     }
 
-    let statuses = {'web':'Loading...', 'api':'Loading...'};
-
     let web = document.getElementById('status-website');
     let webst = Date.now();
     this.img = new Image();
 
     this.img.onload = function() {
         let difference = Date.now() - webst;
-        if (difference >= 2000) { web.innerHTML = "Degraded <img src='./icons/degraded.svg'>"; statuses.web = "Degraded"; }
-        else { web.innerHTML = "Online <img src='./icons/online.svg'>"; statuses.web = "Online"; }
+           console.log('web took '+difference);
+
+        if (difference >= 2000) { web.innerHTML = "Degraded <img src='./icons/degraded.svg'>"; }
+        else { web.innerHTML = "Online <img src='./icons/online.svg'>"; }
     };
-    this.img.onerror = function() { web.innerHTML = "Offline <img src='./icons/offline.svg'>"; statuses.web = "Offline"; };
+    this.img.onerror = function() { web.innerHTML = "Offline <img src='./icons/offline.svg'>"; };
 
     this.start = new Date().getTime();
     this.img.src = "https://www.withered.app/assets/media/icon.png";
 
     let api = document.getElementById('status-api');
+    let db = document.getElementById('status-database');
     let apist = Date.now();
     $.ajax({
         url: 'https://api.withered.app',
@@ -31,37 +32,14 @@ function load() {
            let difference = Date.now() - apist;
            console.log('api took '+difference);
 
-           if (difference >= 2000) { api.innerHTML = "Degraded (took "+difference+"ms) <img src='./icons/degraded.svg'>"; statuses.api = "Degraded"; }
-           else { api.innerHTML = "Online <img src='./icons/online.svg'>"; statuses.api = "Online"; }
+           if (difference >= 2000) { api.innerHTML = "Degraded (took "+difference+"ms) <img src='./icons/degraded.svg'>";
+                db.innerHTML = "Degraded <img src='./icons/degraded.svg'>"; }
+           else { api.innerHTML = "Online <img src='./icons/online.svg'>";
+                db.innerHTML = "Online <img src='./icons/online.svg'>"; }
         },
         error: function(result){
-            api.innerHTML = "Offline <img src='./icons/offline.svg'>"; statuses.api = "Offline";
+            api.innerHTML = "Offline <img src='./icons/offline.svg'>";
+            db.innerHTML = "Offline <img src='./icons/offline.svg'>";
         }
     });
-
-    setInterval(()=>{setSummary(JSON.stringify(statuses))}, 250);
-}
-function setSummary(statusesog) {
-    let statuses = JSON.parse(statusesog);
-    let summary = document.getElementById('summary');
-    let e = true;
-    for(let i=0; i<Object.values(statuses).length; i++) {
-        if (Object.values(statuses)[i] === "Loading...") e = false;
-    }
-    if (e === true) {
-        let onlines = 0;
-        let degradeds = 0;
-        let offlines = 0;
-        for(let i=0; i<Object.values(statuses).length; i++) {
-            if (Object.values(statuses)[i] === "Online") { onlines++; }
-            else if (Object.values(statuses)[i] === "Degraded") { degradeds++; }
-            else if (Object.values(statuses)[i] === "Offline") { offlines++; }
-        }
-        if (degradeds === 0 && offlines === 0 && onlines > 0) { summary.innerHTML = "All services online <img src='./icons/online.svg'>"; }
-        else if (onlines === 0 && degradeds === 0 && offlines > 0) { summary.innerHTML = "All services offline <img src='./icons/offline.svg'>"; }
-        else if (onlines >= degradeds && onlines >= offlines) { summary.innerHTML = "Most services online <img src='./icons/online.svg'>"; }
-        else if (degradeds >= onlines && degradeds >= offlines) { summary.innerHTML = "Most services degraded <img src='./icons/degraded.svg'>"; }
-        else if (offlines >= degradeds && offlines >= onlines) { summary.innerHTML = "Most services offline <img src='./icons/offline.svg'>"; }
-        console.log("o: "+onlines+", d: "+degradeds+", of: "+offlines)
-    }
 }
